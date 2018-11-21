442306a1c23cfd816fd763b32fce7bbbc6ff6376
Ivan Molenaar <s1116162@student.windesheim.nl>
2018-11-21 11:00:44 +0100

    Add NUnit

---
+++ PianoApp/PianoAppTest/MetronomeTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using NUnit.Framework;
+
+namespace Goblit.UnitTests
+{
+    [TestFixture]
+    public class WorldLoaderTests
+    {
+
+        [Test]
+        public void test()
+        {
+
+        }
+    }
+}
